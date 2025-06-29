2303d1be7c84a47a8c46bea6e98c1c155dbb4d42
Kannan <kannan.gopakumar.04@gmail.com>
2025-06-29 20:23:41 +0530

    Initial commit excluding .vs folder

---
+++ Application/Commands/CreateJobCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace JobFinder.API.Application.Commands
+{
+    public class CreateJobCommand: IRequest<int>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Company { get; set; }
+        public string[] Skills { get; set; }
+
+    }
+}
---
+++ Controllers/JobsController.cs
@@ -0,0 +1,26 @@
+﻿using JobFinder.API.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobFinder.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class JobsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public JobsController(IMediator mediator) 
+        { 
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateJob([FromBody] CreateJobCommand command)
+        {
+            var JobId = await _mediator.Send(command);
+            return Ok(JobId);
+        }
+
+    }
+}
