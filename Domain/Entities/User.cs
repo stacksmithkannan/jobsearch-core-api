b37f82e891b94ecdfaccdbfa0361d926a7abad7e
Kannan <kannan.gopakumar.04@gmail.com>
2025-07-01 15:37:24 +0530

    adding jwt 1

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace JobFinder.API.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+
+    }
+}
