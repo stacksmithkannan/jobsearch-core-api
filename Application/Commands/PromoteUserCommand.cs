4834154fe5194ae90347f574c25598c8eb320e3e
Kannan <kannan.gopakumar.04@gmail.com>
2025-07-02 23:20:35 +0530

    added default admin user creation when app starts

---
+++ Application/Commands/PromoteUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace JobFinder.API.Application.Commands
+{
+    public record PromoteUserCommand(int userId):IRequest<string>;
+}
---
+++ Application/Handlers/PromoteUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using JobFinder.API.Application.Commands;
+using JobFinder.API.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobFinder.API.Application.Handlers
+{
+    public class PromoteUserHandler : IRequestHandler<PromoteUserCommand, string>
+    {
+        public readonly ApplicationDbContext _context;
+
+        public PromoteUserHandler(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<string> Handle(PromoteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == request.userId,cancellationToken);
+            if (user == null)
+                return "User not found";
+
+            user.Role = "Admin";
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return "User promoted to Admin";
+        }
+    }
+}
