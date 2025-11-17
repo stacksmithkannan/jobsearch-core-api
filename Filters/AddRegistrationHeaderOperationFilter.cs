9106af5efe56e1d548ab1553612f7e8a0d8efcb2
Kannan <kannan.gopakumar.04@gmail.com>
2025-11-17 21:02:16 +0530

    enabled unauthorized registration with key

---
+++ Filters/AddRegistrationHeaderOperationFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace JobFinder.API.Filters
+{
+    public class AddRegistrationHeaderOperationFilter : IOperationFilter
+    {
+        public void Apply (OpenApiOperation operation,OperationFilterContext context)
+        {
+            if (context.ApiDescription.RelativePath.Contains("auth/register", StringComparison.OrdinalIgnoreCase))
+            {
+                operation.Parameters.Add(new OpenApiParameter
+                {
+                    Name = "X-REG-KEY",
+                    In = ParameterLocation.Header,
+                    Required = false,
+                    Schema = new OpenApiSchema { Type = "string"},
+                    Description = "Secret key required for registration"
+                });
+
+            }
+        }
+    }
+}
